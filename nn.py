f37b6eb4f2abde6f0af97268d001891df624103f
unknown <ksng@kth.se>
2022-06-23 00:23:38 +0200

    some start

--- nn.py
+++ nn.py
@@ -1,22 +1,40 @@
-import enum
 import numpy as np
 
 #input layer, hidden layer, output layer
+#universal approximation theorem
 
-class nn:
+class NN:
     def __init__(self, input_size, hidden_x, hidden_y, output_size):
         self.input_size = input_size
         self.hidden_x = hidden_x
         self.hidden_y = hidden_y
         self.output_size = output_size
 
-        self.input_layer = [0] * self.input
-        self.hidden_layer = [[np.random.seed(0) for _ in range(self.hidden_y)] for _ in range(self.hidden_x)]
-        self.output_layer = [0] * self.output
+        np.random.seed(0)
+
+        self.input_layer = [0] * self.input_size
+        self.hidden_layer = [[round(np.random.uniform(0, 1.0), 1) for _ in range(self.hidden_y)] for _ in range(self.hidden_x)]
+        self.output_layer = [0] * self.output_size
 
     def print_layers(self):
         for i in self.hidden_layer:
             print(i)
     
-    def calculate(self, values):
-        for index, input_neuron in enumerate(self.input_layer):
+    def MSE(self, actual, expected):
+        error = 0
+        for a, e in zip(actual, expected):
+            error += (a - e) ** 2
+        return error / len(actual)
+        
+    #Matrix mult
+    def calculate(self, inp_values):
+        output = 0
+        for index, inp_neuron in enumerate(inp_values):
+            output += inp_neuron * self.hidden_layer[0][index]
+        return output
+    
+    def train(self, all_values):
+        outputs = []
+        for values in all_values:
+            outputs.append(self.calculate(values))
+        return outputs
--- run_nn.py
+++ run_nn.py
@@ -1,8 +1,48 @@
-import nn
+from nn import NN
+import sys
+import numpy as np
+
+#inspired from:
+#https://sirupsen.com/napkin/neural-net
+
+#MSE: SUM(obs(i) - pred(i))^2 / obs
 
 def run_nn():
-    inp, x, y, out = map(int, input())
-    nn = nn(inp, x, y, out)
+    try:
+        inp, x, y, out = map(int, input().split(" "))
+        nn = NN(inp, x, y, out)
+    except:
+        if inp is None:
+            print("inp missing")
+        if x is None:
+            print("x missing")
+        if y is None:
+            print("y missing")
+        if out is None:
+            print("out missing")
+        print("Unknown exception")
+    
+    data, exp = create_data()
+    print(data[0:10])
+    print(exp[0:10])
+    trained = nn.train(data)
+    print(trained[0:10])
+    print(nn.MSE(trained, exp))
+
+
+#Creates data, 1000 rectangles (white - black)
+#Also gets average of rectangles
+def create_data():
+    rectangles = []
+    rectangles_average = []
+    for _ in range(0, 1000):
+        rectangle = [round(np.random.random(), 1), round(np.random.random(), 1), round(np.random.random(), 1), round(np.random.random(), 1)]
+
+        rectangles.append(rectangle)
+        rectangles_average.append(sum(rectangle) / 4)
+    
+    return rectangles, rectangles_average
+        
     
 
 if __name__ == "__main__":
